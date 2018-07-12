2c452657a8d61cfb056b5a3e3489397ff79e10c5
Gabriel Laliberté <gabriel.laliberte.2@ulaval.ca>
2018-07-12 12:07:57 -0400

    Updated network settings for local testing

--- eos-webapp/src/store.js
+++ eos-webapp/src/store.js
@@ -9,9 +9,9 @@ let store = new Vuex.Store({
     network: {
       protocol: 'http',
       blockchain: 'eos',
-      host: 'mainnet.eoscalgary.io',
-      port: 80,
-      chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+      host: '127.0.0.1',
+      port: 8888,
+      chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
     }
   },
 
